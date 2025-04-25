d538d5b27a097695049ebcbe7b1e56dcc6873e1e
lucasbrito3001 <lucas.brito3001@gmail.com>
2025-04-24 21:08:01 -0300

    updating vue

--- src/services/converter.js
+++ src/services/converter.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import MockCryptos from "./mockCryptos";
-const API_URL = "https://cryptos-converter.lucasdbrito.com";
+const API_URL = "https://cryptos-converter.lucasdbrito.com/api";
 
 const ConverterApi = {
 	getCryptos: async function () {
-		let response = axios.get(API_URL + '/cryptocurrency/listings/latest')
+		let response = axios.get(API_URL + '/cryptocurrency/latest')
 		// let response = { data: { status: 200, data: MockCryptos } };
 		return response;
 	},
