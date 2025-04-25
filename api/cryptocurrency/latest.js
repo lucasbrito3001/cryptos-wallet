d59904fadbfb2a8ed2cca2f2f8aa2a05b8920021
lucasbrito3001 <lucas.brito3001@gmail.com>
2025-04-24 21:29:17 -0300

    updating vue

--- api/cryptocurrency/latest.js
+++ api/cryptocurrency/latest.js
@@ -1,11 +1,13 @@
 export async function GET(request) {
 	try {
-		let { data } = await fetch(process.env.CMKTC_URL + "cryptocurrency/listings/latest", {
+		let response = await fetch(process.env.CMKTC_URL + "cryptocurrency/listings/latest", {
 			method: "GET",
 			headers: {
 				"X-CMC_PRO_API_KEY": process.env.CMKTC_KEY,
 			},
 		});
+
+        const data = await response.json()
 
         console.log(data)
 
